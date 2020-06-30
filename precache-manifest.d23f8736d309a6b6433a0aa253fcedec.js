2a0d3e13ea83a8a53780ed61245062539c843e15
Muhammed Hasan Ince <mhasanince@gmail.com>
2020-06-30 18:01:26 +0300

    Updates

---
+++ precache-manifest.d23f8736d309a6b6433a0aa253fcedec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aac5b9fcd0af9cd635699e13f21f893",
+    "url": "/expense-income-tracker-app/index.html"
+  },
+  {
+    "revision": "701674039421c69a48b2",
+    "url": "/expense-income-tracker-app/static/css/main.94003254.chunk.css"
+  },
+  {
+    "revision": "56b9efa5767ad62d50e5",
+    "url": "/expense-income-tracker-app/static/js/2.d8edba2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-income-tracker-app/static/js/2.d8edba2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "701674039421c69a48b2",
+    "url": "/expense-income-tracker-app/static/js/main.6e601ec5.chunk.js"
+  },
+  {
+    "revision": "26ad21f828aa1044997c",
+    "url": "/expense-income-tracker-app/static/js/runtime-main.fba820ff.js"
+  }
+]);
