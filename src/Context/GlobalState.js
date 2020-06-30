b79a0983c4567e91738757d29f450c5151420adf
Muhammed Hasan Ince <mhasanince@gmail.com>
2020-06-30 22:25:50 +0300

    update local storage

--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -3,23 +3,23 @@ import AppReducer from './AppReducer.js';
 
 // Initial State
 
-const initialState = {
-  transactions: [
-    // { id: 1, text: 'Flower', amount: -20 },
-    // { id: 2, text: 'Salary', amount: 300 },
-    // { id: 3, text: 'Book', amount: -10 },
-    // { id: 4, text: 'Camer', amount: -150 },
-  ],
-};
+// const initialState = {
+//   transactions: [
+//     // { id: 1, text: 'Flower', amount: -20 },
+//     // { id: 2, text: 'Salary', amount: 300 },
+//     // { id: 3, text: 'Book', amount: -10 },
+//     // { id: 4, text: 'Camer', amount: -150 },
+//   ],
+// };
 
 // Create Context
 
-export const GlobalContext = createContext(initialState);
+export const GlobalContext = createContext();
 
 // Provider Component
 
 export const GlobalProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AppReducer, initialState, () => {
+  const [state, dispatch] = useReducer(AppReducer, [], () => {
     const localData = localStorage.getItem('transactions');
     return localData ? JSON.parse(localData) : { transactions: [] };
   });
